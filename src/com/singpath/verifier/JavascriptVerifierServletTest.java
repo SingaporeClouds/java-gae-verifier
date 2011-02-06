78f50242c6eeffb76d9697cc2cad78e4d61a9f10
Tay Ray Chuan <rctay89@gmail.com>
2011-02-06 18:13:28 +0800

    JSVerifierTest: test lang error

--- src/com/singpath/verifier/JavascriptVerifierServletTest.java
+++ src/com/singpath/verifier/JavascriptVerifierServletTest.java
@@ -1,14 +1,47 @@
 package com.singpath.verifier;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 public class JavascriptVerifierServletTest {
 
-	private final JavascriptVerifierServlet inst = new JavascriptVerifierServlet();
+	private JavascriptVerifierServlet servlet;
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private StringWriter response_writer;
+	private Map<String, String> parameters;
+
+	@Before
+	public void setUp() throws IOException {
+		parameters = new HashMap<String, String>();
+		servlet = new JavascriptVerifierServlet();
+		request = mock(HttpServletRequest.class);
+		response = mock(HttpServletResponse.class);
+		response_writer = new StringWriter();
+
+		when(request.getParameter(anyString())).thenAnswer(new Answer<String>() {
+			public String answer(InvocationOnMock invocation) {
+				return parameters.get((String) invocation.getArguments()[0]);
+			}
+		});
+		when(response.getWriter()).thenReturn(new PrintWriter(response_writer));
+	}
 
 	private static void assertJSONHasKey(String exp_key, String exp_val, String act_str)
 		throws JSONException {
@@ -20,9 +53,9 @@ public class JavascriptVerifierServletTest {
 	@Test
 	public void testParseJsSimple() throws Exception {
 		assertJSONHasKey("solved", "true",
-			inst.parseJs("", "assert_equal(1,1);"));
+			servlet.parseJs("", "assert_equal(1,1);"));
 		assertJSONHasKey("solved", "true",
-			inst.parseJs(
+			servlet.parseJs(
 				"function add(a, b) { return a+b; }",
 				"assert_equal(add(1,2),3);"));
 	}
@@ -30,11 +63,21 @@ public class JavascriptVerifierServletTest {
 	@Test
 	public void testParseJsWhitespace() throws Exception {
 		assertJSONHasKey("solved", "true",
-			inst.parseJs(
+			servlet.parseJs(
 				"function add(a, b) {\n"
 				+ "	return a+b;\n"
 				+ "}",
 				"assert_equal(add(1,2),3);"));
+	}
+
+	@Test
+	public void testNonExistentFunction() throws Exception {
+		parameters.put("script", "");
+		parameters.put("tests", "assert_equal(add(1,2),3);");
+		servlet.doPost(request, response);
+		assertEquals(
+			"{\"errors\":\"java.lang.ArrayIndexOutOfBoundsException: 1\"}\r\n",
+			response_writer.toString());
 	}
 
 }
