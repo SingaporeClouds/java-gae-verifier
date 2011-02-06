44758a1804e5d6aaf06556fe420b660220445a51
Tay Ray Chuan <rctay89@gmail.com>
2011-02-06 23:45:57 +0800

    JavaVerifier: whitespace changes

--- src/com/singpath/verifier/JavaVerifierServlet.java
+++ src/com/singpath/verifier/JavaVerifierServlet.java
@@ -23,20 +23,20 @@ import static org.junit.Assert.*;
 @SuppressWarnings("serial")
 public class JavaVerifierServlet extends HttpServlet {
 	private Logger logger = Logger.getLogger(this.getClass().getName());
-	
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException{
 		this.doGet(req, resp);
 	}
-	
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		String userStr = req.getParameter("jsonrequest");
-		
+
 		logger.info("userStr:" + userStr);
 		//userStr = URLDecoder.decode(userStr, "UTF-8");
 		//mock a json test string
-		
+
 		try
 		{
 			//HashMap hash = new HashMap();
@@ -48,7 +48,7 @@ public class JavaVerifierServlet extends HttpServlet {
 			logger.info("userStr:" + userStr);
 			JSONObject jsonObj = new JSONObject(userStr);
 			resp.getWriter().println(parseJava(jsonObj.getString("solution"), jsonObj.getString("tests")));
-			
+
 		}
 		catch(Exception e)
 		{
@@ -57,19 +57,19 @@ public class JavaVerifierServlet extends HttpServlet {
 			em.put("errors", e.toString());
 			resp.getWriter().println(new JSONObject(em).toString());
 		}
-	    
+
 		resp.setContentType("application/json");
-		
-		
+
+
 	}
-	
+
 	public HashMap<String, String> getTests(String s) throws Exception
 	{
 		HashMap<String, String> tests = new HashMap<String, String>();
 		String[] test = s.split(">>>");
 		for(int i = 1; i < test.length; i++)
 		{
-			BufferedReader reader = new BufferedReader(new StringReader(test[i]));					      
+			BufferedReader reader = new BufferedReader(new StringReader(test[i]));
 			String key = reader.readLine().trim();
 			String value = reader.readLine().trim();
 			tests.put(key, value);
@@ -77,30 +77,30 @@ public class JavaVerifierServlet extends HttpServlet {
 		}
 		return tests;
 	}
-	
+
 	public String readJSONString(HttpServletRequest request){
-	   StringBuffer json = new StringBuffer();
-	   String line = null;
-	   try {
-		   BufferedReader reader = request.getReader();
-		   while((line = reader.readLine()) != null) {
-			   json.append(line);
-		   }
-	   }
-	   catch(Exception e) {
-		   logger.info("Error in readJSONString:" + e.toString());
-	   }
-	   return json.toString();
+		StringBuffer json = new StringBuffer();
+		String line = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while((line = reader.readLine()) != null) {
+				json.append(line);
+			}
+		}
+		catch(Exception e) {
+			logger.info("Error in readJSONString:" + e.toString());
+		}
+		return json.toString();
 	}
-	
-	public String parseJava(String script, String tests) throws Exception		
+
+	public String parseJava(String script, String tests) throws Exception
 	{
 		StringBuffer strResult = new StringBuffer();
 		ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
-	    PrintStream output = new PrintStream(outputBuffer);
+		PrintStream output = new PrintStream(outputBuffer);
 		Interpreter interpreter = new Interpreter(null, null, null, false);
 		interpreter.setStrictJava(true);
-		
+
 		try
 		{
 			interpreter.eval("static import org.junit.Assert.*;");
@@ -109,23 +109,23 @@ public class JavaVerifierServlet extends HttpServlet {
 		{
 			e.printStackTrace();
 		}
-		
-		PrintStream out = System.out;
-	      PrintStream err = System.err;
-	      System.setOut(output);
-	      System.setErr(output);
 
-	      try {
-	        // Eval the user text
-	        interpreter.eval(script);
-	      }
-	      finally {
-	        System.setOut(out);
-	        System.setErr(err);
-	      }
-				
+		PrintStream out = System.out;
+		PrintStream err = System.err;
+		System.setOut(output);
+		System.setErr(output);
+
+		try {
+			// Eval the user text
+			interpreter.eval(script);
+		}
+		finally {
+			System.setOut(out);
+			System.setErr(err);
+		}
+
 		output.flush();
-		
+
 		String[] testscripts = tests.split("\n");
 		ArrayList<JSONObject> testResults = new ArrayList<JSONObject>();
 		boolean solved = true;
@@ -150,7 +150,7 @@ public class JavaVerifierServlet extends HttpServlet {
 					resulthash.put("expected", true);
 					resulthash.put("received", false);
 					resulthash.put("call", testscript);
-					resulthash.put("correct", false);		
+					resulthash.put("correct", false);
 					testResults.add(new JSONObject(resulthash));
 					continue;
 				}
@@ -160,11 +160,11 @@ public class JavaVerifierServlet extends HttpServlet {
 					resulthash.put("expected", false);
 					resulthash.put("received", true);
 					resulthash.put("call", testscript);
-					resulthash.put("correct", false);		
+					resulthash.put("correct", false);
 					testResults.add(new JSONObject(resulthash));
 					continue;
-				}	
-				
+				}
+
 				String failS = e.getTarget().getMessage();
 				failS = failS.replace("expected:<", "");
 				failS = failS.replace("> but was:<", ",");
@@ -173,11 +173,11 @@ public class JavaVerifierServlet extends HttpServlet {
 				resulthash.put("expected", ss[0]);
 				resulthash.put("received", ss[1]);
 				resulthash.put("call", testscript);
-				resulthash.put("correct", false);		
+				resulthash.put("correct", false);
 				testResults.add(new JSONObject(resulthash));
 				continue;
 			}
-			
+
 			//correct without exception
 			HashMap<String, Object> resulthash = new HashMap<String, Object>();
 			//resulthash.put("expected", value);
@@ -194,14 +194,14 @@ public class JavaVerifierServlet extends HttpServlet {
 			JSONObject json = new JSONObject(resultjson);
 			strResult.append(json.toString());
 
-		
-		return strResult.toString();
-	}	
 
-	
+		return strResult.toString();
+	}
+
+
 	public static void main(String[] args)
 	{
-				
+
 		int a = 1; //
 		assertEquals(a ,1);
 		String userStr = "%7B%22tests%22%3A+%22+%5Cn%3E%3E%3E+b+%5Cn+2%5Cn%5Cn%5Cn%5Cn%5Cn%22%2C+%22solution%22%3A+%22+%5Cnint+b%3D2%3B%5Cn%5Cn%5Ct%5Cn%5Cn%22%7D";
@@ -213,19 +213,19 @@ public class JavaVerifierServlet extends HttpServlet {
 		userStr = testObj.toString();
 		try
 		{
-			
+
 			JSONObject jsonObj = new JSONObject(userStr);
 			JavaVerifierServlet instance = new JavaVerifierServlet();
-			
+
 			System.out.println(instance.parseJava(jsonObj.getString("solution"), jsonObj.getString("tests")));
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-		
-	
+
+
 
 	}
-	
+
 }
